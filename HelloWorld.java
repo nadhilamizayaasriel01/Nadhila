f8c3d46302c041e0f7638e7a48d3ecdd3c4c5cd8
nadhilamizayaasriel01 <91447672+nadhilamizayaasriel01@users.noreply.github.com>
2021-10-02 23:56:15 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Nadhila Mizaya Asriel");
+		System.out.println("NIM: 6706213078");
+		System.out.println("Kelas: D3RPLA 45 02");
+	}
+}
